bca10a7a78139b65613481f1e0736140a9152d8f
TepNik <tepelin.nikita@gmail.com>
2022-12-15 21:25:42 +0600

    Added OpenApi class, eslinter, prettier, config, and updated dependnses

---
+++ src/openai/openai.ts
@@ -0,0 +1,57 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+import { config } from '../../config.js';
+
+interface ImageGenerationResponse {
+    text: string;
+    imageUrl?: string;
+    b64_json?: string;
+}
+
+export class OpenApi {
+    private openai: OpenAIApi;
+
+    constructor(apiKey?: string) {
+        if (!apiKey) {
+            apiKey = config.openAiApi;
+        }
+
+        const configuration = new Configuration({
+            apiKey,
+        });
+        this.openai = new OpenAIApi(configuration);
+    }
+
+    async getImagesFromText(
+        initialPrompt: string,
+        n = 10,
+    ): Promise<ImageGenerationResponse[]> {
+        const textResponse = await this.openai.createCompletion({
+            model: 'text-davinci-001',
+            prompt: initialPrompt,
+            temperature: 0.4,
+            max_tokens: Math.floor(2048 / n),
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            n: n,
+        });
+
+        const result: ImageGenerationResponse[] = [];
+        for (let i = 0; i < textResponse.data.choices.length; ++i) {
+            const text =
+                textResponse.data.choices[i].text?.replace('\n', '') ?? '';
+            const imageResponse = await this.openai.createImage({
+                prompt: text,
+            });
+
+            result.push({
+                text,
+                imageUrl: imageResponse.data.data[0].url,
+                b64_json: imageResponse.data.data[0].b64_json,
+            });
+        }
+
+        return result;
+    }
+}
